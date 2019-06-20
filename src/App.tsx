607dbc9f534954cfb6cf1c609c80447b916d1bba
Simonas Karuzas <simonas@not.cat>
2019-06-20 12:12:51 +0300

    Codepush logging

--- src/App.tsx
+++ src/App.tsx
@@ -4,9 +4,7 @@ import './lib/I18n'
 import Navigation from './screens/Navigation'
 import { client } from './lib/GraphQL'
 import Log from './lib/Log'
-import Analytics from 'appcenter-analytics'
-
-Analytics.setEnabled(true)
+import codePush, { DownloadProgress } from 'react-native-code-push'
 
 const defaultHandler =
   ErrorUtils.getGlobalHandler && ErrorUtils.getGlobalHandler()
@@ -17,10 +15,38 @@ if (defaultHandler) {
   })
 }
 
-const App = () => (
-  <ApolloProvider client={client}>
-    <Navigation />
-  </ApolloProvider>
-)
+class App extends React.Component {
+  codePushStatusDidChange(status: codePush.SyncStatus) {
+    switch (status) {
+      case codePush.SyncStatus.CHECKING_FOR_UPDATE:
+        Log.info('Checking for updates.', 'Codepush')
+        break
+      case codePush.SyncStatus.DOWNLOADING_PACKAGE:
+        Log.info('Downloading package.', 'Codepush')
+        break
+      case codePush.SyncStatus.INSTALLING_UPDATE:
+        Log.info('Installing update.', 'Codepush')
+        break
+      case codePush.SyncStatus.UP_TO_DATE:
+        Log.info('Up-to-date.', 'Codepush')
+        break
+      case codePush.SyncStatus.UPDATE_INSTALLED:
+        Log.info('Update installed.', 'Codepush')
+        break
+    }
+  }
+
+  codePushDownloadDidProgress(progress: DownloadProgress) {
+    // console.log(progress.receivedBytes + ' of ' + progress.totalBytes + ' received.');
+  }
+
+  render() {
+    return (
+      <ApolloProvider client={client}>
+        <Navigation />
+      </ApolloProvider>
+    )
+  }
+}
 
 export default App
