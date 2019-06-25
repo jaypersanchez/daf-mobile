4fcb07412e1027350effff243b614d6615d849d3
Jason Healy <hello@jasonhealy.io>
2019-06-25 11:34:13 +0100

    Update tests

--- src/navigators/__tests__/Navigation.test.tsx
+++ src/navigators/__tests__/Navigation.test.tsx
@@ -1,6 +1,6 @@
 import 'react-native'
 import React from 'react'
-import Navigation from '../Navigation'
+import Navigation from '../'
 import { render } from 'react-native-testing-library'
 jest.useFakeTimers()
 
