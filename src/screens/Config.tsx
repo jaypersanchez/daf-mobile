1aafef79e1fe9ac79e6c94ced0bb30ebf9c3ca41
Jason Healy <hello@jasonhealy.io>
2019-07-03 19:18:03 +0100

    Fix config screen crash

--- src/screens/Config.tsx
+++ src/screens/Config.tsx
@@ -9,16 +9,25 @@ import Config from 'react-native-config'
 export default () => {
   const configs = Object.keys(Config.getConstants())
 
+  // tslint:disable-next-line:no-console
+  console.log(configs)
+
   return (
     <Screen scrollEnabled={true}>
       <Container>
         <Section>
-          {configs.map((key: string, i: number) => (
-            <ListItem key={key} subTitle={key} last={i === configs.length - 1}>
-              {Config[key].substring(0, 25) +
-                (Config[key].length > 25 ? '...' : '')}
-            </ListItem>
-          ))}
+          {configs.map((key: string, i: number) => {
+            const value = Config[key].toString()
+            return (
+              <ListItem
+                key={key}
+                subTitle={key}
+                last={i === configs.length - 1}
+              >
+                {value.substring(0, 25) + (value.length > 25 ? '...' : '')}
+              </ListItem>
+            )
+          })}
         </Section>
       </Container>
     </Screen>
