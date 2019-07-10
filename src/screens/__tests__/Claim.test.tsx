f3f575f5347849cf14bc4c3139fdc32fde8acc26
Jason Healy <hello@jasonhealy.io>
2019-07-10 18:29:22 +0100

    Add claim view test

---
+++ src/screens/__tests__/Claim.test.tsx
@@ -0,0 +1,14 @@
+import 'react-native'
+import React from 'react'
+import Claim from '../Claim'
+import { render } from 'react-native-testing-library'
+
+const navigation = {
+  goBack: jest.fn(),
+}
+
+it('renders correctly', () => {
+  // @ts-ignore
+  const tree = render(<Claim navigation={navigation} />).toJSON()
+  expect(tree).toMatchSnapshot()
+})
