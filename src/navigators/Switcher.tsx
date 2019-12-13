14adf473be1fdc3d32261dea9ba26c175510e824
Jason Healy <hello@jasonhealy.io>
2019-12-13 16:02:30 +0000

    feat: Add identity creation to switcher

--- src/navigators/Switcher.tsx
+++ src/navigators/Switcher.tsx
@@ -1,9 +1,22 @@
 import React from 'react'
 import { ScrollView } from 'react-native-gesture-handler'
-import { BottomSheet, ListItem, Avatar, BottomSnap } from '@kancha/kancha-ui'
+import {
+  BottomSheet,
+  ListItem,
+  Avatar,
+  BottomSnap,
+  Container,
+  Button,
+  Icon,
+} from '@kancha/kancha-ui'
 import { useQuery, useMutation, useApolloClient } from '@apollo/react-hooks'
-import { Theme } from '../theme'
-import { GET_MANAGED_IDENTITIES, SET_VIEWER } from '../lib/graphql/queries'
+import { Theme, Colors } from '../theme'
+import {
+  GET_MANAGED_IDENTITIES,
+  SET_VIEWER,
+  IMPORT_IDENTITY,
+  CREATE_IDENTITY,
+} from '../lib/graphql/queries'
 
 interface Identity {
   did: string
@@ -20,6 +33,13 @@ const Switcher: React.FC<SwitcherProps> = ({ id }) => {
   const client = useApolloClient()
   const { data } = useQuery(GET_MANAGED_IDENTITIES)
   const [setViewer] = useMutation(SET_VIEWER)
+  const [createIdentity] = useMutation(CREATE_IDENTITY, {
+    refetchQueries: [{ query: GET_MANAGED_IDENTITIES }],
+    variables: {
+      type: 'rnEthr',
+    },
+  })
+
   const managedIdentities =
     data && data.managedIdentities && data.managedIdentities
 
@@ -80,6 +100,19 @@ const Switcher: React.FC<SwitcherProps> = ({ id }) => {
                   </ListItem>
                 )
               })}
+          <Container padding alignItems={'center'} dividerTop>
+            <Button
+              iconButton
+              buttonText={'Create identity'}
+              icon={
+                <Icon
+                  color={Colors.CONFIRM}
+                  icon={{ name: 'ios-add-circle', iconFamily: 'Ionicons' }}
+                />
+              }
+              onPress={() => createIdentity()}
+            />
+          </Container>
         </ScrollView>
       )}
     </BottomSheet>
