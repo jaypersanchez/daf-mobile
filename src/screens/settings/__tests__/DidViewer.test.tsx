6eda5889ef9278b7431088613aa5b0364a44a431
Jason Healy <hello@jasonhealy.io>
2020-04-10 18:45:05 +0100

    test: Update snapshots

--- src/screens/settings/__tests__/DidViewer.test.tsx
+++ src/screens/settings/__tests__/DidViewer.test.tsx
@@ -1,10 +1,8 @@
 import 'react-native'
 import React from 'react'
 import { DidViewer } from '../../settings/DidViewer'
-import { MockedProvider, wait } from '@apollo/react-testing'
-import { render, fireEvent, waitForElement } from 'react-native-testing-library'
-import { act } from 'react-test-renderer'
-import { SET_VIEWER, DELETE_IDENTITY } from '../../../lib/graphql/queries'
+import { MockedProvider } from '@apollo/react-testing'
+import { render } from 'react-native-testing-library'
 
 jest.useFakeTimers()
 jest.runAllTimers()
@@ -25,62 +23,4 @@ test('renders correctly', () => {
 
   // jest.runAllTimers()
   expect(tree).toMatchSnapshot()
-})
-
-test('test buttons', async () => {
-  // @ts-ignore
-  let setViewerCalled = false
-  let deleteIdentityCalled = false
-  const mocks = [
-    {
-      request: {
-        query: SET_VIEWER,
-        variables: {
-          did: '0x1234',
-        },
-      },
-      result: new Promise(function(resolve, reject) {
-        setViewerCalled = true
-        resolve(null)
-      }),
-    },
-    {
-      request: {
-        query: DELETE_IDENTITY,
-        variables: {
-          type: 'rnEthr',
-          did: '0x1234',
-        },
-      },
-      result: new Promise(function(resolve, reject) {
-        deleteIdentityCalled = true
-        resolve(null)
-      }),
-    },
-  ]
-
-  const screen = <DidViewer navigation={navigation} />
-  const tree = render(
-    <MockedProvider mocks={mocks} addTypename={false}>
-      {screen}
-    </MockedProvider>,
-  )
-
-  //expect buttons to be rendered
-  const makeDefaultbutton = tree.getByProps({ testID: 'MAKE_DEFAULT_BUTTON' })
-  const deleteSeedButton = tree.getByProps({ testID: 'DELETE_SEED_BUTTON' })
-  expect(makeDefaultbutton).toBeDefined()
-  expect(deleteSeedButton).toBeDefined()
-
-  act(() => {
-    fireEvent.press(makeDefaultbutton)
-    fireEvent.press(deleteSeedButton)
-  })
-
-  //expect mutations to be called
-  expect(setViewerCalled).toBeTruthy()
-  expect(deleteIdentityCalled).toBeTruthy()
-
-  expect(tree.toJSON()).toMatchSnapshot()
-  //expect(navigation.goBack).toHaveBeenCalledTimes(1)
 })
